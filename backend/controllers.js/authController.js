25f1a11bded27c9af445dffc47a94efed2f8bffd
Chaim <c0507634153@gmail.com>
2026-09-17 10:34:13 +0300

    Implement login route

--- backend/controllers.js/authController.js
+++ backend/controllers.js/authController.js
@@ -1,5 +1,5 @@
 import { addUser, getUserByEmail } from "../repositories/usersRepo.js";
-import { hashPassword } from "../services/authServices.js";
+import { hashPassword, verifyPassword } from "../services/authServices.js";
 
 export const register = async (req, res) => {
     let { username, email, password } = req.body;
@@ -10,7 +10,7 @@ export const register = async (req, res) => {
     if (!(username && email && password))
         throw Object.assign(new Error(), {
             status: 400,
-            message: "Name & enail & password are required",
+            message: "Name & email & password are required",
         });
     if (await getUserByEmail(email))
         throw Object.assign(new Error(), {
@@ -18,11 +18,22 @@ export const register = async (req, res) => {
             message: "Enail already exists",
         });
 
-    const hashedPassword =await hashPassword(password);
+    const hashedPassword = await hashPassword(password);
     await addUser({ username, email, password: hashedPassword });
     res.status(201).json({
         success: true,
         message: "User registed successfully",
     });
 };
-export const login = () => {};
+export const login = async (req, res) => {
+    let { password, email } = req.body;
+    if (!(email && password))
+        throw Object.assign(new Error(), {
+            status: 400,
+            message: "Email & password are required",
+        });
+    email = email.trim();
+    password = password.trim();
+    const token = await verifyPassword(email, password);
+    res.status(201).json({ success: true, token });
+};
--- backend/services/authServices.js
+++ backend/services/authServices.js
@@ -1,5 +1,20 @@
 import bcrypt from "bcrypt";
+import { getUserByEmail } from "../repositories/usersRepo.js";
+import jwt from 'jsonwebtoken'
+
+const {JWT_SECRET} = process.env
 
 export const hashPassword = (password) => {
     return bcrypt.hash(password, 10);
+};
+
+export const verifyPassword = async (email, password) => {
+    const { id, password: hashedPassword } = await getUserByEmail(email);
+    const isMatch = await bcrypt.compare(password, hashedPassword);
+    if (!isMatch)
+        throw Object.assign(new Error(), {
+            status: 401,
+            message: "Email or password are incorrect",
+        });
+    return jwt.sign({id, email}, JWT_SECRET)
 };
